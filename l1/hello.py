590dc67179fab235830ae83e176010a06c56a0ab
jatin1998nair <35593920+jatin1998nair@users.noreply.github.com>
2019-08-27 12:53:42 +0530

    Add files via upload

---
+++ l1/hello.py
@@ -0,0 +1,14 @@
+a = int(input("enter the value of a:"))
+b = int(input("enter the value of b:"))
+c = int(a+b)
+d = int(a*b)
+e = int(a-b)
+f = float(a/b)
+print ("addition of ",a,"and",b,c)
+print ("multiply is ",d)
+print ("difference is",e)
+print ("division is",f)
+
+
+
+
